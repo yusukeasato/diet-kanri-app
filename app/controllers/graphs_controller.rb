a848680a72b93d47f9fb41164a6bcadfe376c456
Yusuke Asato <yusukeasato@YAsMBP.local>
2020-08-27 04:35:43 +0900

    データベースに保存できるようにする

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,14 +1,29 @@
 class GraphsController < ApplicationController
   def index
     gon.weight_records = Graph.chart_data(current_user)
+    # 記録済みの日付データ
+    gon.recorded_dates = current_user.graphs.map(&:date)
   end
 
   def create
-
+    @graph = current_user.graphs.build(graph_params)
+    date = @graph.date.strftime('%Y/%-m/%-d')
+    if @graph.save
+      flash[:notice] = "#{date}の記録を追加しました"
+    else
+      flash[:alert] = 'エラーが発生しました'
+    end
+    redirect_to root_path
   end
 
   def update
 
+  end
+
+  private
+
+  def graph_params
+    params.require(:graph).permit(:date, :weight)
   end
 
 end
