3df0effeb597e1fa92ee94d1451255ff6e280d9a
Yusuke Asato <yusukeasato@YAsMBP.local>
2020-08-27 03:59:46 +0900

    処理をまとめる関数を作成して共通化

--- app/assets/javascripts/graphs.js
+++ app/assets/javascripts/graphs.js
@@ -1,7 +1,14 @@
 document.addEventListener('turbolinks:load', () => {
   // '2020-01-12'のような文字列から，Javascriptの日付オブジェクトを取得する関数
-  // setHoursを使用しないと，時差の影響で0時にならないため注意！
   const convertDate = (date) => new Date(new Date(date).setHours(0, 0, 0, 0))
+
+  // 日付の古い方・新しい方を取得する関数
+  const minDate = (date1, date2) => (date1 < date2) ? date1 : date2
+  const maxDate = (date1, date2) => (date1 > date2) ? date1 : date2
+
+  // データの初日・最終日
+  const START_DATE = convertDate(gon.weight_records[0].date)
+  const END_DATE = convertDate(gon.weight_records[gon.weight_records.length - 1].date)
 
   const TODAY = convertDate(new Date())
   const A_WEEK_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate() - 6)
@@ -11,7 +18,6 @@ document.addEventListener('turbolinks:load', () => {
 
   // グラフを描く場所を取得
   const chartWeightContext = document.getElementById("chart-weight").getContext('2d')
-  let chartWeight
 
   // 期間を指定してグラフを描く
   const drawGraph = (from, to) => {
@@ -57,21 +63,37 @@ document.addEventListener('turbolinks:load', () => {
       }
     }
 
-    if (!chartWeight) {
-      // グラフが存在しないときは，作成する
-      chartWeight = new Chart(chartWeightContext, {
-        type: 'line',
-        data: weightData,
-        options: weightOption
-      })
-    } else {
-      // グラフが存在するときは，更新する
-      chartWeight.data = weightData
-      chartWeight.options = weightOption
-      chartWeight.update()
-    }
+    new Chart(chartWeightContext, {
+      type: 'line',
+      data: weightData,
+      options: weightOption
+    })
   }
 
+  // 引数の日付から今日までのグラフを描く関数
+  const drawGraphToToday = (from) => {
+    // データが存在する範囲に修正
+    from = maxDate(from, START_DATE)
+    let to = minDate(TODAY, END_DATE)
+    drawGraph(from, to)
+  }
+
+  // 過去◯週間のグラフを描くボタン
+  document.getElementById('a-week-button').addEventListener('click', () => {
+    drawGraphToToday(A_WEEK_AGO)
+  })
+
+  document.getElementById('two-weeks-button').addEventListener('click', () => {
+    drawGraphToToday(TWO_WEEKS_AGO)
+  })
+
+  document.getElementById('a-month-button').addEventListener('click', () => {
+    drawGraphToToday(A_MONTH_AGO)
+  })
+
+  document.getElementById('three-months-button').addEventListener('click', () => {
+    drawGraphToToday(THREE_MONTHS_AGO)
+  })
   // グラフの初期表示
-  drawGraph(A_WEEK_AGO, TODAY)
+  drawGraphToToday(A_WEEK_AGO)
 })
