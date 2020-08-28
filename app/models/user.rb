a4d5365edde2beb150b1d20efa9c0f688addd4c2
Yusuke Asato <yusukeasato@YAsMBP.local>
2020-08-28 18:12:24 +0900

    アップデート

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :graphs, dependent: :destroy       
+  has_many :graphs, dependent: :destroy
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+  end
 end
